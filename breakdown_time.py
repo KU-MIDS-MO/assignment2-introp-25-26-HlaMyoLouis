7bb39d1dedd12f610547b0388f5e527fc6d884d9
Hla Myo <hlamyo.louis@gmail.com>
2025-11-09 23:31:38 +0100

    done

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,12 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if not isinstance(seconds, int) or seconds<0:
+        return -1
+    if seconds==0:
+        return {}
+    result={}
+    for unit in [3600,60,1]:
+        count=seconds//unit
+        if count>0:
+            result[unit]=count
+        seconds %= unit
+    return result
--- swap_ends.py
+++ swap_ends.py
@@ -1,4 +1,10 @@
 def swap_ends(L, k):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if not L or k <= 0 or k > len(L)//2:
+        return (L.copy(),0)
+    new_list=L.copy()
+    n=len(new_list)
+    if n%2==0:
+        new_list[:k],new_list[-k:]=new_list[-k:],new_list[:k]
+    else:
+        new_list[:k],new_list[-k:]=new_list[-k:],new_list[:k]
+    return (new_list,k)
